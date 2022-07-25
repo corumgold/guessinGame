62c9212d7b74e74b23923a06b8defa3537ea3f67
Corum Gold <goldcorum@gmail.com>
2022-07-25 17:34:37 -0500

    Add guess boxes

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -5,6 +5,7 @@ let guesses = document.getElementById("past-guesses");
 let notifier = document.getElementById("notifier");
 let guessInput = document.getElementById("guess-input");
 let hints = document.getElementById("hints");
+let guessBlocks = document.getElementById("guess-blocks");
 
 function generateWinningNumber() {
   return Math.floor(Math.random() * (100 - 1 + 1) + 1);
@@ -27,7 +28,7 @@ function shuffle(array) {
 }
 
 class Game {
-  constructor(guess) {
+  constructor() {
     this.playersGuess = null;
     this.pastGuesses = [];
     this.winningNumber = generateWinningNumber();
@@ -46,9 +47,24 @@ class Game {
       alert("That is an invalid guess.");
     } else {
       this.playersGuess = num;
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(`${this.playersGuess}`));
-      guesses.appendChild(li);
+      let guessCt = this.pastGuesses.length;
+      switch (true) {
+        case guessCt === 0:
+          document.getElementById("guess-1").innerHTML = num;
+          break;
+        case guessCt === 1:
+          document.getElementById("guess-2").innerHTML = num;
+          break;
+        case guessCt === 2:
+          document.getElementById("guess-3").innerHTML = num;
+          break;
+        case guessCt === 3:
+          document.getElementById("guess-4").innerHTML = num;
+          break;
+        case guessCt === 4:
+          document.getElementById("guess-5").innerHTML = num;
+          break;
+      }
       notifier.innerHTML = this.checkGuess();
       guessInput.value = "";
     }
@@ -58,7 +74,7 @@ class Game {
     if (this.pastGuesses.includes(this.playersGuess)) {
       return "You have already guessed that number.";
     } else if (this.playersGuess === this.winningNumber) {
-      return "You Win!";
+      return winScreen();
     } else if (this.pastGuesses.length < 4) {
       this.pastGuesses.push(this.playersGuess);
       let diff = this.difference();
@@ -81,7 +97,7 @@ class Game {
         updateText += " Choose something smaller!";
       } else updateText += " Choose something larger!";
       return updateText;
-    } else return "You Lose.";
+    } else return loseScreen();
   }
 
   provideHint() {
@@ -95,10 +111,43 @@ class Game {
 
 function newGame() {
   let game = new Game();
-  guesses.innerHTML = "";
   notifier.innerHTML = "";
+  let nodes = guessBlocks.childNodes;
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].innerHTML = "-";
+  }
   hints.innerHTML = game.provideHint();
+  hintButton.style.display = "block";
+  submitButton.style.display = "block";
+  notifier.style.display = "block";
+  guessBlocks.style.color = "#264653";
   return game;
+}
+
+function winScreen() {
+  guessBlocks.style.color = "#2a9d8f";
+  document.getElementById("guess-1").innerHTML = "W";
+  document.getElementById("guess-2").innerHTML = "I";
+  document.getElementById("guess-3").innerHTML = "N";
+  document.getElementById("guess-4").innerHTML = ":";
+  document.getElementById("guess-5").innerHTML = ")";
+  hintButton.style.display = "none";
+  submitButton.style.display = "none";
+  notifier.style.display = "none";
+  guess.style.display = "none";
+}
+
+function loseScreen() {
+  guessBlocks.style.color = "#e76f51";
+  document.getElementById("guess-1").innerHTML = "L";
+  document.getElementById("guess-2").innerHTML = "O";
+  document.getElementById("guess-3").innerHTML = "S";
+  document.getElementById("guess-4").innerHTML = "E";
+  document.getElementById("guess-5").innerHTML = "!";
+  hintButton.style.display = "none";
+  submitButton.style.display = "none";
+  notifier.style.display = "none";
+  guess.style.display = "none";
 }
 
 let game = newGame();
